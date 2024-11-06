fe07367aef066d495b1a255f684b4fefceb73090
nigoleto <nigoleto@gmail.com>
2024-11-06 11:30:19 +0900

    test_file_3

--- src/main/java/io/security/springsecuritymaster/Test.java
+++ src/main/java/io/security/springsecuritymaster/Test.java
@@ -2,8 +2,21 @@ package io.security.springsecuritymaster;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Scanner;
+
 @Service
 public class Test {
 
-
+    public void test() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Please enter your password: ");
+        String password = scanner.nextLine();
+        System.out.println("Please enter your email: ");
+        String email = scanner.nextLine();
+        System.out.println("Please enter your age: ");
+        int age = scanner.nextInt();
+        scanner.close();
+    }
 }
