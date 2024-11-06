291ef4cecdca4a325ffd6a310f84c16d998e27ce
nigoleto <nigoleto@gmail.com>
2024-11-06 10:53:48 +0900

    test_file_add

--- src/main/java/io/security/springsecuritymaster/Test.java
+++ src/main/java/io/security/springsecuritymaster/Test.java
@@ -1,13 +1,9 @@
 package io.security.springsecuritymaster;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
-@RestController
+@Service
 public class Test {
 
-    @GetMapping("/")
-    public String home() {
-        return "Hello World";
-    }
+
 }
