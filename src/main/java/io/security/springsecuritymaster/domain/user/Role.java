87b22e3a2bf1f288324b4d48bd0cdb7b7840ee78
nigoleto <nigoleto@gmail.com>
2024-11-22 09:05:06 +0900

    feat(domain): Enum Role 생성

---
+++ src/main/java/io/security/springsecuritymaster/domain/user/Role.java
@@ -0,0 +1,12 @@
+package io.security.springsecuritymaster.domain.user;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + name();
+    }
+}
