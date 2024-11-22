af9164bdda730a20e31346aff2a5074e80797483
nigoleto <nigoleto@gmail.com>
2024-11-22 23:43:36 +0900

    feat(Entity): @EnableJpaAuditing 추가

--- src/main/java/io/security/springsecuritymaster/SpringSecurityMasterApplication.java
+++ src/main/java/io/security/springsecuritymaster/SpringSecurityMasterApplication.java
@@ -2,8 +2,10 @@ package io.security.springsecuritymaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringSecurityMasterApplication {
 
     public static void main(String[] args) {
