74570fd28b7a3002b0434da08e204ba887991003
nigoleto <nigoleto@gmail.com>
2024-11-24 03:36:13 +0900

    feat(Clothes): ResponseDto 필드추가

--- src/main/java/io/security/springsecuritymaster/domain/clothes/ClothesResponseDto.java
+++ src/main/java/io/security/springsecuritymaster/domain/clothes/ClothesResponseDto.java
@@ -3,22 +3,28 @@ package io.security.springsecuritymaster.domain.clothes;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class ClothesResponseDto {
 
+    private Long id;
     private String nickname;
 //    private Attach filePath;
 //    private String thumbnailUrl;
     private String title;
     private String description;
+    private LocalDateTime createdAt;
     private int viewCount;
 
     public static ClothesResponseDto toDto(Clothes clothes) {
         return ClothesResponseDto.builder()
+                .id(clothes.getId())
                 .nickname(clothes.getUser().getNickname())
                 .title(clothes.getTitle())
                 .description(clothes.getDescription())
+                .createdAt(clothes.getCreatedAt())
                 .viewCount(clothes.getViewCount())
                 .build();
     }
