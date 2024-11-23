58425ef3afbd3672a37fb9f3a08e68f9723e4233
nigoleto <nigoleto@gmail.com>
2024-11-23 23:04:44 +0900

    feat(common): ErrorCode 추가

--- src/main/java/io/security/springsecuritymaster/common/exception/ErrorCode.java
+++ src/main/java/io/security/springsecuritymaster/common/exception/ErrorCode.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+    USER_NOT_FOUND(404, "user not found"),
+    USER_DEACTIVATED(400, "user deactivated"),
+    USER_SUSPENDED(400, "user suspended"),
     CLOTHES_NOT_FOUND(404, "clothes not found"),
     ALREADY_DELETED(400, "Already deleted entity"),
     ALREADY_LIKED(400, "Already liked post");
