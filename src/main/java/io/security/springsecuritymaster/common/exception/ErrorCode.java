780495e88847b5c3ee4339b59a53fb2e2b947695
nigoleto <nigoleto@gmail.com>
2024-11-25 02:25:26 +0900

    feat(Common): 예외추가

--- src/main/java/io/security/springsecuritymaster/common/exception/ErrorCode.java
+++ src/main/java/io/security/springsecuritymaster/common/exception/ErrorCode.java
@@ -9,6 +9,9 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "user not found"),
     USER_DEACTIVATED(400, "user deactivated"),
     USER_SUSPENDED(400, "user suspended"),
+    DUPLICATE_EMAIL(409, "Email is already in use"),
+    DUPLICATE_NICKNAME(409, "Nickname is already in use"),
+    INVALID_PASSWORD(400, "invalid password"),
     CLOTHES_NOT_FOUND(404, "clothes not found"),
     ALREADY_DELETED(400, "Already deleted entity"),
     ALREADY_LIKED(400, "Already liked post");
