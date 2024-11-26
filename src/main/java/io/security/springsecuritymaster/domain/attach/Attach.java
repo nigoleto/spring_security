3864cb0b36610069491f7ae462ee9d5ad686eaf0
nigoleto <nigoleto@gmail.com>
2024-11-26 17:00:57 +0900

    feat(Attach): 첨부파일 필드추가

--- src/main/java/io/security/springsecuritymaster/domain/attach/Attach.java
+++ src/main/java/io/security/springsecuritymaster/domain/attach/Attach.java
@@ -1,19 +1,33 @@
 package io.security.springsecuritymaster.domain.attach;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import io.security.springsecuritymaster.domain.clothes.Clothes;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Attach {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long fileId;
+
+    private String fileName;
+
+    private String fileType;
+
+    private String fileUrl;
 
     @ManyToOne
     @JoinColumn(name = "clothes_id", nullable = false)
+    @JsonBackReference
     private Clothes clothes;
 
 }
---
+++ src/main/java/io/security/springsecuritymaster/repository/AttachRepository.java
@@ -0,0 +1,7 @@
+package io.security.springsecuritymaster.repository;
+
+import io.security.springsecuritymaster.domain.attach.Attach;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttachRepository extends JpaRepository<Attach, Long> {
+}
